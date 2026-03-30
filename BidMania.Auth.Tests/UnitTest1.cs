47e0ecf16109fef7a627ecdbe2f6f23116344fef
Mehmet Erdem Morgül <mehmetlehayat08@gmail.com>
2026-03-30 20:52:31 +0300

    İskelet Yapısı Kurulmuştur

---
+++ BidMania.Auth.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace BidMania.Auth.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ BidMania.Dispatcher.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace BidMania.Dispatcher.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
