786e8cc36a40f66f62190f705b1ed7555e54a134
mustafakvizilti <kucukviziltimustafa@gmail.com>
2026-03-31 00:11:13 +0300

    dispatcher test yapıldı
    Token gönderilmediğinde 401 Unauthorized dönmesini bekleyen ilk test senaryosu eklendi.

--- BidMania.Dispatcher.Tests/UnitTest1.cs
+++ BidMania.Dispatcher.Tests/UnitTest1.cs
@@ -1,11 +1,26 @@
-﻿namespace BidMania.Dispatcher.Tests
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+﻿using BidMania.Dispatcher; 
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using Xunit;
 
-        }
+namespace BidMania.Dispatcher.Tests;
+
+public class AuthTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client;
+
+    public AuthTests(WebApplicationFactory<Program> factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task Token_Gonderilmezse_401_Unauthorized_Donmeli()
+    {
+
+        var response = await _client.GetAsync("/api/products");
+
+
+        Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
     }
 }
