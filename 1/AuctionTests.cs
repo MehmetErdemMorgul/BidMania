fc1a2d92a8c50ae0abf29f63d146b3fd875d6bab
mustafakvizilti <kucukviziltimustafa@gmail.com>
2026-03-31 02:19:42 +0300

    auction testi red flag
    auction service için yeni ihale oluşturulursa 201 created kodu dönecek test red flag verdi

---
+++ 1/AuctionTests.cs
@@ -0,0 +1,33 @@
+﻿using BidMania.AuctionService;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System.Net;
+using System.Net.Http.Json;
+using Xunit;
+
+namespace BidMania.Auction.Tests;
+
+public class AuctionTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client;
+
+    public AuctionTests(WebApplicationFactory<Program> factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task Yeni_Ihale_Olusturulursa_201_Created_Donmeli()
+    {
+        var newAuction = new
+        {
+            ItemName = "Antika Saat",
+            StartingPrice = 1000,
+            EndTime = DateTime.Now.AddDays(1)
+        };
+
+        var response = await _client.PostAsJsonAsync("/api/auctions", newAuction);
+
+        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+    }
+}
