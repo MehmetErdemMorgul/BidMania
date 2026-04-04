b68acedc281198ce865fec52a8eeb1fa854953f8
mustafakvizilti <kucukviziltimustafa@gmail.com>
2026-04-04 20:42:03 +0300

    sanırsam son bugfix

--- BidMania.AuctionService/Program.cs
+++ BidMania.AuctionService/Program.cs
@@ -3,17 +3,14 @@ using BidMania.AuctionService.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// MongoDB 
 builder.Services.Configure<AuctionDatabaseSettings>(
     builder.Configuration.GetSection("AuctionDatabaseSettings"));
-
 
 builder.Services.AddSingleton<AuctionRepository>();
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
-
 
 app.Use(async (context, next) =>
 {
@@ -27,11 +24,11 @@ app.Use(async (context, next) =>
     await next();
 });
 
-app.UseHttpsRedirection();
+
 app.UseAuthorization();
 app.MapControllers();
 
+Console.WriteLine("AUCTION SERVICE .NET 9 RUNNING");
 app.Run();
 
-// TESTLER İÇİN ŞART!
 public partial class Program { }
