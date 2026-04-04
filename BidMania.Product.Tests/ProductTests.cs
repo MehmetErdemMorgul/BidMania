0b7a2a9f97e74d2e01da4b77c42d8f0fb28a8e3e
Mehmet Erdem Morgül <mehmetlehayat08@gmail.com>
2026-04-04 16:49:24 +0300

    Ürün ekleme testi başarıyla yazıldı red flag alındı

---
+++ BidMania.Product.Tests/ProductTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System.Net;
+using System.Net.Http.Json;
+using Xunit;
+
+
+namespace BidMania.Product.Tests;
+
+public class ProductTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client;
+    private const string InternalKey = "Kocaeli41_Secret";
+
+    public ProductTests(WebApplicationFactory<Program> factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task Yeni_Urun_Eklendiginde_201_Created_Donmeli()
+    {
+        
+        var product = new { Name = "Test Ürünü", Price = 100.50m };
+
+        _client.DefaultRequestHeaders.Clear();
+        _client.DefaultRequestHeaders.Add("X-Internal-Key", InternalKey);
+
+        var response = await _client.PostAsJsonAsync("/api/products", product);
+        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+    }
+}
